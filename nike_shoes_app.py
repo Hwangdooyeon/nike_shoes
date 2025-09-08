ea471e35ba52705f359c48d3ac6120b8bb1e5895
Hwang doo yeon <yueuch102@naver.com>
2025-09-08 16:16:49 +0900

    Update nike_shoes_app.py

--- nike_shoes_app.py
+++ nike_shoes_app.py
@@ -44,7 +44,7 @@ try:
     if rescode == 200:
         response_body = response.read()
         result_json = json.loads(response_body.decode('utf-8'))
-#응답 바이트를 .decode('utf-8')로 문자열(str)로 변환(JSON은 보통 UTF-8)한 뒤 그 문자열을 json.loads로 파싱해 result_json에 담음
+#응답 바이트를 .decode('utf-8')로 문자열(str)로 변환(JSON은 보통 UTF-8)한 뒤 그 문자열을 json.loads로 파싱해 result_json에 담음 ( 비율(%)가 아니라 지수라서 100을 넘어가지 않음 )
     else:
         st.error(f"API Error Code: {rescode}")
         st.stop()
